f939019b9e45184535d482461f08f82f464cf488
Akhil-design-jpg <vakhil838@gmail.com>
2025-03-24 12:05:00 +0530

    Initial commit

---
+++ location_App/src/App.jsx
@@ -0,0 +1,15 @@
+import Location from './location/Location'
+import './App.css'
+function App() {
+ 
+  return (
+    <>
+      <div>
+        Hello
+        <Location />
+      </div>
+    </>
+  )
+}
+
+export default App
